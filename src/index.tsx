feef707327706e0f284016c31d8f6873e92ed901
Aleee D <aleee.datos@gmail.com>
2022-04-08 00:01:50 -0300

    intentando hacer andar Express para el CORS

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import { Provider } from 'react-redux'
-
 import store from 'store'
+import cors from 'cors'
+import resp from 'express'
+
+const app = resp()
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+  res.send('hello world')
+})
 
 ReactDOM.render(
   <Provider store={store}>
